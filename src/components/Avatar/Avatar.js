7d7a2d3dba715e0f976af3b4516a1df03149336c
Carlos Cucurullo <carlos.c@medicalwebexperts.com>
2018-09-04 16:54:39 -0300

    diferent sizes for avatars

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -5,25 +5,47 @@ import { withTheme } from '../../theme';
 const propTypes = {
   image: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['small', 'normal', 'large']),
+};
+
+const defaultProps = {
+  size: 'normal',
 };
 
 const Avatar = ({
-  image, alt,
-}) => (
-  <div>
-    <style jsx>
-      {`
-        img{
-          border-radius: 100%;
-          display: block;
-          width: 100px;
-          height: auto;
-        }
-      `}
-    </style>
-    <img src={image} alt={alt}/>
-  </div>
-);
+  image, alt, size,
+}) => {
+  // check sizes
+  let avatarWidth = null;
+  switch (size) {
+    case 'small':
+      avatarWidth = '60px';
+      break;
+    case 'normal':
+      avatarWidth = '100px';
+      break;
+    case 'large':
+      avatarWidth = '200px';
+      break;
+  }
+
+  return (
+    <div>
+      <style jsx>
+        {`
+          img{
+            border-radius: 100%;
+            display: block;
+            width: ${avatarWidth};
+          }
+        `}
+      </style>
+      <img src={image} alt={alt} size={size}/>
+    </div>
+  );
+};
 
 Avatar.propTypes = propTypes;
+Avatar.defaultProps = defaultProps;
+
 export default withTheme(Avatar);
