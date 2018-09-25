502416b20f64d062156ba72ad7122255b97068ca
Roberto-Torrico <roberto.t@medicalwebexperts.com>
2018-09-25 16:07:38 -0300

    Deleted mock data in file and added external mock data

--- src/components/ListItem/ListItem.test.js
+++ src/components/ListItem/ListItem.test.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 import ListItem from './ListItem';
+import { basicProvider } from '../../../mocks/providers';
+import { basicLocation } from '../../../mocks/locations';
 
 const providerProps = {
-  type: 'provider',
-  avatar: 'https://placehold.it/350x350',
-  name: 'John Doe',
-  title: 'MD',
-  specialty: 'Family Medicine',
-  location: 'Mansfield Clinic',
-  description: 'This is a description, like a subtitle.',
+  ...basicProvider,
   action: '',
   actionText: 'View Profile',
 };
 
 const locationProps = {
-  type: 'location',
-  avatar: 'https://placehold.it/350x350',
-  name: 'Albuquerque Urgent Care',
-  address: 'Street 1',
-  city: 'Albuquerque',
-  state: 'New Mexico',
+  ...basicLocation,
   action: '',
   actionText: 'View Location',
 };
