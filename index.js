43dee22030189a2b8d11118ac49eb232a341debf
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-10-24 16:44:05 -0300

    moved index

---
+++ index.js
@@ -0,0 +1,39 @@
+import {
+  H1, H2, H3, H4, H5, H6, Text,
+} from './src/components/Typography';
+import Button from './src/components/Button/Button';
+import Input from './src/components/Input/Input';
+import Select from './src/components/Select/Select';
+import Avatar from './src/components/Avatar/Avatar';
+import Card from './src/components/Card/Card';
+import SearchForm from './src/components/SearchForm/SearchForm';
+import Icon from './src/components/Icon/Icon';
+import List from './src/components/List/List';
+import Information from './src/components/Information/Information';
+import Map from './src/components/Map/Map';
+import ListItem from './src/components/ListItem/ListItem';
+import Data from './src/components/Data/Data';
+import Profile from './src/components/Profile/Profile';
+
+export {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Text,
+  Button,
+  Input,
+  Avatar,
+  Select,
+  Card,
+  SearchForm,
+  ListItem,
+  Icon,
+  List,
+  Information,
+  Map,
+  Data,
+  Profile,
+};
