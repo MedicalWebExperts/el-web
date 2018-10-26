5d4fcb364734ea14953bf9e74719d4ac22dbffe7
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-10-26 16:53:50 -0300

    fix in media query for ssr

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,6 +1,26 @@
 export default {
-  isMobile: () => window.matchMedia('(max-width: 480px)').matches,
-  isTablet: () => window.matchMedia('(min-width:481px) and (max-width: 768px)').matches,
-  isTabletLanscape: () => window.matchMedia('(min-width:769px) and (max-width: 992px)').matches,
-  isFullScreen: () => window.matchMedia('(min-width:993px) and (max-width: 1200px)').matches,
+  isMobile: () => {
+    if (window) {
+      return window.matchMedia('(max-width: 480px)').matches;
+    }
+    return false;
+  },
+  isTablet: () => {
+    if (window) {
+      return window.matchMedia('(min-width:481px) and (max-width: 768px)').matches;
+    }
+    return false;
+  },
+  isTabletLanscape: () => {
+    if (window) {
+      return window.matchMedia('(min-width:769px) and (max-width: 992px)').matches;
+    }
+    return false;
+  },
+  isFullScreen: () => {
+    if (window) {
+      return window.matchMedia('(min-width:993px) and (max-width: 1200px)').matches;
+    }
+    return true;
+  },
 };
