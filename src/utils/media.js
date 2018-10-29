cba681a849a3ae0d19aab95d59f8730e3607e3f5
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-10-29 09:23:33 -0300

    fixed media for server rendering

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,24 +1,30 @@
+const canUseDOM = !!(
+  typeof window !== 'undefined'
+  && window.document
+  && window.document.createElement
+);
+
 export default {
   isMobile: () => {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(max-width: 480px)').matches;
     }
     return false;
   },
   isTablet: () => {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:481px) and (max-width: 768px)').matches;
     }
     return false;
   },
   isTabletLanscape: () => {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:769px) and (max-width: 992px)').matches;
     }
     return false;
   },
   isFullScreen: () => {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:993px) and (max-width: 1200px)').matches;
     }
     return true;
