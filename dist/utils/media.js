ee4cf21b1b6ff255c74a22fe65521a3a1086d2d0
Ignacio Martin <ignacio.m@medicalwebexperts.com>
2018-10-29 09:36:20 -0300

    added build

--- dist/utils/media.js
+++ dist/utils/media.js
@@ -3,27 +3,29 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement);
+
 exports.default = {
   isMobile: function isMobile() {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(max-width: 480px)').matches;
     }
     return false;
   },
   isTablet: function isTablet() {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:481px) and (max-width: 768px)').matches;
     }
     return false;
   },
   isTabletLanscape: function isTabletLanscape() {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:769px) and (max-width: 992px)').matches;
     }
     return false;
   },
   isFullScreen: function isFullScreen() {
-    if (window) {
+    if (canUseDOM) {
       return window.matchMedia('(min-width:993px) and (max-width: 1200px)').matches;
     }
     return true;
